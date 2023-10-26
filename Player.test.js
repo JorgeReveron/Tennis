543c5357026a990918fe03c87375f728e7d03bab
JorgeReveron <jorgealexanderclases@gmail.com>
2023-10-26 19:42:22 +0100

    add birthday,age,category to class Player

--- Player.test.js
+++ Player.test.js
@@ -37,3 +37,40 @@ test("crear jugador female por poner un genero extraño", () =>{
     expect(jugador1.name).toBe("Ana");
     expect(jugador1.gender).toBe("female");
 });
+
+test("Crear jugador con fecha nacimiento valida", () =>{
+    const jugador1 = new Player("Ana",2,"female","13/04/1999");
+    expect(jugador1.birthday).toBe("13/04/1999");
+});
+
+test("Crear jugador con fecha nacimiento no valida", () =>{
+    const jugador1 = new Player("Ana",2,"female","kkfu");
+    expect(jugador1.birthday).not.toBe("kkfu");
+    expect(jugador1.birthday).toBeUndefined();
+    expect(jugador1.age).toBeUndefined();
+});
+
+test("Crear jugador con fecha nacimiento valida", () =>{
+    const jugador1 = new Player("Ana",2,"female","28/10/2000");
+    expect(jugador1.age).toBe(22);
+});
+
+test("jugador benjamin",()=>{
+    const jugador = new Player("Pepe",1,"male","12/12/2016");
+    expect(jugador.category).toBe("Benjamin");
+});
+
+test("jugador cadet",()=>{
+    const jugador = new Player("Pepe",1,"male","1/1/2010");
+    expect(jugador.category).toBe("Cadet");
+});
+
+test("jugador junior",()=>{
+    const jugador = new Player("Pepe",1,"male","1/1/2004");
+    expect(jugador.category).toBe("Junior");
+});
+
+test("jugador senior",()=>{
+    const jugador = new Player("Pepe",1,"male","12/12/2000");
+    expect(jugador.category).toBe("Senior");
+});
